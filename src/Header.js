d8428d4c00d75e98d7d0081acad0db385f4b6298
Ali Habib <edualihabib@gmail.com>
2023-02-21 08:09:15 +0200

    some changes

---
+++ src/Header.js
@@ -0,0 +1,18 @@
+
+function Header() {
+    return (
+        <header>
+            <div>
+                <h1>Little Lemon</h1>
+                <h4>Chicago</h4>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                <butten>Reserve a Table</butten>
+            </div>
+            <img 
+            src="#"
+            alt="food img"
+            />
+        </header>
+    );
+}
+export default Header;
---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+function Nav() {
+    return (
+        <nav>
+            <img
+            src="https://www.48hourslogo.com/oss/works/2022/01/08/0372776096/115168_45900_1de80bd0-6665-4562-894b-a6e9f3fcfe5a.jpg"
+            alt="logo"
+            />
+            <ul>
+                <li><a href="#home">home</a></li>
+                <li><a href="#about">about</a></li>
+                <li><a href="#menu">menu</a></li>
+                <li><a href="#reservation">reservation</a></li>
+                <li><a href="#order_online">order online</a></li>
+                <li><a href="#login">login</a></li>
+            </ul>
+        </nav>
+    );
+}
+export default Nav;
