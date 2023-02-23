a6efce3ae8fe616228281d86ae3302e7ffacded8
Ali Habib <edualihabib@gmail.com>
2023-02-23 11:28:41 +0200

    Chicago html bulde

--- src/components/Chicago.js
+++ src/components/Chicago.js
@@ -1,7 +1,16 @@
 function Chicago() {
     return(
-        <header>
-        
+        <header className="header">
+        <div>
+                <h1>Little Lemon</h1>
+                <h4>Chicago</h4>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                <butten>Reserve a Table</butten>
+            </div>
+            <img
+            src="#"
+            alt="food img"
+            />
         </header>
     )
 }
